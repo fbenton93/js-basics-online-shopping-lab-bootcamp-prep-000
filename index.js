e77f35bf3baab5b24b8ce9ef37502aa5f8569d7c
Forrest Benton <elliottbenton@gmail.com>
2018-07-02 15:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
         output += `${cart[i].itemName} at $${cart[i].itemPrice}, `
       }
     }
-    return "In your cart, you have" + output
+    return "In your cart, you have " + output
   
   }
   
