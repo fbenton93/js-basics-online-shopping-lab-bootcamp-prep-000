bf8861a2462a129d3ae37e958d3ad70caeafb6e5
Forrest Benton <elliottbenton@gmail.com>
2018-07-02 15:16:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var output = new Object();
+  var output = {};
   if (cart.length === 0) {
     return "Your shopping cart is empty."
   } else if (cart.length === 1) {
