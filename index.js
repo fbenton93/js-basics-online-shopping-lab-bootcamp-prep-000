d7def1720dfc15164c12b71053575c7faa0d65df
Forrest Benton <elliottbenton@gmail.com>
2018-07-02 15:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,5 +60,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cc) {
-  
+  var cost = total()
+  if (cc === undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  return `Your total cost is ${cost}, which will be charged to the card ${cc}.`
 }
